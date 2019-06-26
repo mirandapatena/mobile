2b6c9c29b45b08c6db45f81371798481b9499fbf
sterbartolini <lesterbartolini@gmail.com>
2019-06-27 00:05:54 +0800

    lester changes

--- src/components/RegularUser.js
+++ src/components/RegularUser.js
@@ -380,7 +380,9 @@ export default class RegularUser extends Component {
        incidentID:'', 
        responderLat: null, 
        volunteerLat: null,
-       markerCoords: null}); 
+       markerCoords: null,
+       hasResponderAlerted: false, // has reponder 
+      }); 
 
  }
 
@@ -400,6 +402,18 @@ export default class RegularUser extends Component {
  regularListen.update({
  incidentID: '',
  });
+
+ this.setState({ 
+   isSettled: false, 
+   isIncidentReady: false, 
+   incidentLocation:'',
+   pinUpdate:false, 
+   // incidentID:'', 
+   responderLat: null, 
+   volunteerLat: null,
+   markerCoords: null,
+   hasResponderAlerted: false, // has reponder 
+  }); 
 
  }
 
@@ -1067,6 +1081,18 @@ export default class RegularUser extends Component {
  );
  }
 
+ let polylinemarker = null;
+
+ polylinemarker = (
+   <MapViewDirections
+   origin={{latitude: this.state.latitude, longitude: this.state.longitude}}
+   destination={{latitude: this.state.markerLat, longitude: this.state.markerLng}}
+   apikey={apiKey}
+   strokeWidth={3}
+   strokeColor="hotpink"
+   />
+ )
+
  var markerVolunteer = null;
  if (this.state.volunteerLat) {
  markerVolunteer = (
@@ -1136,19 +1162,8 @@ export default class RegularUser extends Component {
  >
  {getUserLocation}
 
- {/* <Polyline
- coordinates={this.state.pointCoords}
- strokeWidth={4}
- strokeColor="red"
- /> */}
- {this.state.isSettled === true ? null : <MapViewDirections
- origin={{latitude: this.state.latitude, longitude: this.state.longitude}}
- destination={{latitude: this.state.markerLat, longitude: this.state.markerLng}}
- apikey={apiKey}
- strokeWidth={3}
- strokeColor="hotpink"
- />
- }
+
+ {this.state.isSettled === true ? null : polylinemarker}
  {this.state.isSettled === true ? null : marker}
  {this.state.isSettled === true ? null : markerResponder}
  {this.state.isSettled === true ? null : markerVolunteer}
